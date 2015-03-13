ef8ee6dbbf4128143287c78fa8cacb43089b467d
Cyrill Schumacher <cyrill@schumacher.fm>
2015-03-13 11:09:55 +1100

    Add table taxonomies

--- app/code/community/BlueVisionTec/GoogleShoppingApi/sql/googleshoppingapi_setup/install-0.0.1.php
+++ app/code/community/BlueVisionTec/GoogleShoppingApi/sql/googleshoppingapi_setup/install-0.0.1.php
@@ -1,21 +1,5 @@
 <?php
-/**
- *
- * @category	BlueVisionTec
- * @package    BlueVisionTec_GoogleShoppingApi
- * @copyright   Copyright (c) 2012 Magento Inc. (http://www.magentocommerce.com)
- * @copyright   Copyright (c) 2015 BlueVisionTec UG (haftungsbeschränkt) (http://www.bluevisiontec.de)
- * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
 
-/**
- * GoogleShopping install
- *
- * @category	BlueVisionTec
- * @package    BlueVisionTec_GoogleShoppingApi
- * @author      Magento Core Team <core@magentocommerce.com>
- * @author      BlueVisionTec UG (haftungsbeschränkt) <magedev@bluevisiontec.eu>
- */
 /** @var $installer Mage_Core_Model_Resource_Setup */
 
 $installer = $this;
